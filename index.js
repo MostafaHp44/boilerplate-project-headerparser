1708bdcd37c75da7a14e0999eb3281975236d21e
MoustafaHp <mostafahp44@gmail.com>
2023-05-23 16:29:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'].split(',')[0],
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+})
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000,  ()=> {
   console.log('Your app is listening on port ' + listener.address().port);
 });
